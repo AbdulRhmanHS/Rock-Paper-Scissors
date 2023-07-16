99e81b112a8473001a52a6b53fcbcce09746f176
AbdulRhman <bodahassan12@gmail.com>
2023-07-16 15:30:54 +0300

    Make the code look more clean and correct some comments

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 //First we need to make a function that gets random computer choice
 //of rock, paper or scissors.
+
 //We'll use a random number function that returns any number from 0, 1 and 2.
+
 //if the number is 0 it will be siccsors, if the number is 1 it will paper,
 //if the number is 2 it will be rock.
+
+
 function getComputerChoice()
 {
     let number = Math.floor(Math.random() * 3);
@@ -10,11 +14,17 @@ function getComputerChoice()
     if (number === 1) return "paper";
     if (number === 2) return "rock";
 }
+
+
 //We need to take input form the player by a prompt function to write rock, 
 //paper or sicssors.
+
 //We'll make it as a function that returns the player input as a string.
+
 //We need to make sure that this input is case insensitive to make the game
 //more playable.
+
+
 function getPlayerChoice()
 {
     let c = prompt("Enter Rock, Paper or Scissors!", "");
@@ -25,19 +35,24 @@ function getPlayerChoice()
     }
     else
     {
-        return "error";
+        return "error"; //Validates the player input.
     }
 }
-//Then we need to make a function that operates the game at which it
-//combares the player and computer input and declares the winner.
 
-//The function will take two parameters one will be combared.
+
+//Then we need to make a function that operates the game at which it
+//compares the player and computer input and declares the winner.
+
+//The function will take two parameters which will be compared later.
 
 //We will make two conditions one to check the winning cases like when scissors beats
 //paper and when paper beats rock and when rock beats scissors.
 
 //the second one if both inputs are equal it will return draw.
-//if nothing satisafied these two conditions it will return lose cases.
+
+//if nothing satisafies these two conditions it will return a lose case.
+
+
 function playerRound(playerSelection, computerSelection)
 {
     if (((playerSelection === "scissors") && (computerSelection === "paper"))
@@ -59,7 +74,9 @@ function playerRound(playerSelection, computerSelection)
         console.log(`You lose!, ${computerSelection} beats ${playerSelection}.`);
     }
 }
-//We'll make a game() function that makes the game last for 5 rounds.
+
+
+//Makes the game last for 5 rounds.
 function game()
 {
     for (let i = 0; i < 5; i++)
@@ -69,5 +86,6 @@ function game()
     }
 }
 
-//Execute the game.
+
+//Executing the game.
 game();
